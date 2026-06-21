fbced506d8e53ddbab3b54e5e84fb13ed47ed042
Wahid Bux <wahidmari2004@gmail.com>
2026-06-21 20:59:50 +0500

    Initial commit - Django project setup

---
+++ tweet/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tweet
+from .forms import TweetForm 
+
+
+def index(request):
+    return render(request, 'index.html')
+
+# Display all tweets
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+# Create a tweet
+def tweet_create(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+
+            return redirect('tweet_list')
+
+    else:
+        form = TweetForm()
+
+    return render(request, 'tweet_form.html', {'form': form})
+
+# Edit a tweet
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(
+        Tweet,
+        pk=tweet_id,
+        user=request.user
+    )
+
+    if request.method == "POST":
+        form = TweetForm(
+            request.POST,   
+            request.FILES,
+            instance=tweet
+        )
+
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+
+            return redirect('tweet_list')
+
+    else:
+        form = TweetForm(instance=tweet)
+
+    return render(request, 'tweet_form.html', {'form': form})
+
+# Delete a tweet
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(
+        Tweet,
+        pk=tweet_id,
+        user=request.user
+    )
+
+    if request.method == "POST":
+        tweet.delete()
+        return redirect('tweet_list')
+
+    return render(
+        request,
+        'tweet_confirm_delete.html',
+        {'tweet': tweet}
+    )
+    
